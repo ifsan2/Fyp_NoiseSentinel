29daefb0774eaedb47ced0929a99947597416191
codeWithMoez <221073@students.au.edu.pk>
2025-09-28 20:52:45 +0500

    Generic Repo

---
+++ src/NoiseSentinel.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoiseSentinel.DAL.Contexts;
+using NoiseSentinel.DAL.IRepositories;
+
+namespace NoiseSentinel.DAL.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly NoiseSentinelDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(NoiseSentinelDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
