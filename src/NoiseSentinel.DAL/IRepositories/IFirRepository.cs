b26d4fd8f120398138184625a9625dc1a85316eb
codeWithMoez <221073@students.au.edu.pk>
2025-09-28 22:15:57 +0500

    Added IRepos

---
+++ src/NoiseSentinel.DAL/IRepositories/IFirRepository.cs
@@ -0,0 +1,12 @@
+﻿using NoiseSentinel.DAL.Models;
+
+namespace NoiseSentinel.DAL.IRepositories
+{
+    public interface IFirRepository : IGenericRepository<Fir>
+    {
+        Task<Fir?> GetFirByFirNumberAsync(string firNumber);
+        Task<IEnumerable<Fir>> GetFirsByStationIdAsync(int stationId);
+        Task<Fir?> GetFirByChallanIdAsync(int challanId);
+        Task<Fir?> GetFirWithDetailsAsync(int firId);
+    }
+}
---
+++ src/NoiseSentinel.DAL/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using NoiseSentinel.DAL.IRepositories;
+using NoiseSentinel.DAL.Models;
+using NoiseSentinel.DAL.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace NoiseSentinel.DAL
+{
+    // The Unit of Work interface defines a contract for managing all repositories
+    // and committing all changes to the database in a single transaction.
+    public interface IUnitOfWork : IDisposable
+    {
+        // Specific Repositories
+        IChallanRepository Challans { get; }
+        IAccusedRepository Accuseds { get; }
+        IVehicleRepository Vehicles { get; }
+        IPoliceofficerRepository PoliceOfficers { get; }
+        IFirRepository FIRs { get; }
+        ICaseRepository Cases { get; }
+
+        // Generic Repositories for other entities
+        IGenericRepository<Emissionreport> EmissionReports { get; }
+        IGenericRepository<Violation> Violations { get; }
+        IGenericRepository<User> Users { get; }
+        IGenericRepository<Role> Roles { get; }
+        IGenericRepository<Policestation> PoliceStations { get; }
+        IGenericRepository<Judge> Judges { get; }
+        IGenericRepository<Court> Courts { get; }
+        IGenericRepository<Courttype> CourtTypes { get; }
+        IGenericRepository<Iotdevice> IoTDevices { get; }
+        IGenericRepository<Casestatement> CaseStatements { get; }
+
+        // The method that will save all changes made within this unit of work.
+        Task<int> CompleteAsync();
+    }
+}
+
