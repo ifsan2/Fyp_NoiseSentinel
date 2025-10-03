7ff0be9ed54c2847fedea3e68fe776f873a910d7
codeWithMoez <221073@students.au.edu.pk>
2025-10-03 12:30:34 +0500

    Reset Repositories

--- src/NoiseSentinel.DAL/IRepositories/IAccusedRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using NoiseSentinel.DAL.Models;
-
-namespace NoiseSentinel.DAL.IRepositories
-{
-    public interface IAccusedRepository : IGenericRepository<Accused>
-    {
-        Task<Accused?> GetAccusedByCnicAsync(string cnic);
-    }
-}
